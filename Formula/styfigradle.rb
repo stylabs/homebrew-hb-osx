d2b8360731d6e4e659fca231baa61f7ccc6a60b7
Jobin Lawrance <jobin@stylabs.in>
2018-06-05 22:05:45 +0530

    fix class name

--- Formula/styfigradle.rb
+++ Formula/styfigradle.rb
@@ -1,4 +1,4 @@
-class StyfiGradle < Formula 
+class Styfigradle < Formula 
   desc "A command line shortcut to gradle install debug built with minSdk of 23 and launch the app with adb"
   url "https://github.com/jobinlawrance/gradle-styfi/archive/v1.0.0.tar.gz" 
   sha256 "3761354e19058237e93c00cdadff0b64d73a9465d1a05f196a66f382424cdd5b"
