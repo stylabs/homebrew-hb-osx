fe6cd6f067a5912ae1c305f1abcdca8daed430c0
Jobin Lawrance <jobin@stylabs.in>
2018-06-06 16:23:11 +0530

    update gradle-styfi to v1.0.1

--- Formula/styfigradle.rb
+++ Formula/styfigradle.rb
@@ -1,8 +1,8 @@
 class Styfigradle < Formula 
   desc "A command line shortcut to gradle install debug built with minSdk of 23 and launch the app with adb"
-  url "https://github.com/jobinlawrance/gradle-styfi/archive/v1.0.0.tar.gz" 
+  url "https://github.com/jobinlawrance/gradle-styfi/archive/v1.0.1.tar.gz" 
   sha256 "3761354e19058237e93c00cdadff0b64d73a9465d1a05f196a66f382424cdd5b"
-  version "1.0.0"
+  version "1.0.1"
 
   bottle :unneeded
 
