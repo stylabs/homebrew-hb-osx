7ef22176b7b792db47e009f9f16db4083ffaadc2
Jobin Lawrance <jobin@stylabs.in>
2018-06-06 16:44:42 +0530

    fix the sha256 of 1.0.1 artifact

--- Formula/styfigradle.rb
+++ Formula/styfigradle.rb
@@ -1,7 +1,7 @@
 class Styfigradle < Formula 
   desc "A command line shortcut to gradle install debug built with minSdk of 23 and launch the app with adb"
   url "https://github.com/jobinlawrance/gradle-styfi/archive/v1.0.1.tar.gz" 
-  sha256 "3761354e19058237e93c00cdadff0b64d73a9465d1a05f196a66f382424cdd5b"
+  sha256 "983b6ea1ab09a948e7421c32f95c2066df5907f3fd671668212f94043a409c69"
   version "1.0.1"
 
   bottle :unneeded
