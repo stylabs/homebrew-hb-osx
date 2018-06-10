4b32488f55ba5604f3efcf1cae63714131dc8696
Travis CI <travis@travis-ci.org>
2018-06-10 23:11:18 +0000

    Travis build: 51

--- constants.rb
+++ constants.rb
@@ -1,5 +1,5 @@
 module Constants
   URL = "https://api.github.com/repos/jobinlawrance/gradle-styfi/tarball/v1.0.3"
-  SHA = "37fff8ce86e15b380b38ffc18db5ff80b96035620863d2350008370132abe125"
+  SHA = "31191e1803333c4e4c4be83a94579c0d44a3a8a834d4c0297495cd28eba6d27f"
   VERSION = ""
 end
