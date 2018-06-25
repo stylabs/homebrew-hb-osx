b921abbf3113616cfc99a1bd6fe2eeddbf9a056e
Jobin Lawrance <jobinlawrance@gmail.com>
2018-06-25 19:43:25 +0530

    Fix styfigradle

--- constants.rb
+++ constants.rb
@@ -1,5 +1,5 @@
 module Constants
-  URL = ""
-  SHA = ""
-  VERSION = ""
+  URL = "https://github.com/jobinlawrance/gradle-styfi/archive/v1.0.2.tar.gz"
+  SHA = "971979873ac20ae5bc0a26ece21ff35a1f60c276129387f6adddb92d14c2116f"
+  VERSION = "1.0.2"
 end
