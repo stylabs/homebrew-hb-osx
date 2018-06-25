ddd4238d595299b5618f932be07cd50d62dfa837
Jobin Lawrance <jobinlawrance@gmail.com>
2018-06-25 20:11:44 +0530

    update collabb delegate to 0.0.2

--- Formula/collabbdelegate.rb
+++ Formula/collabbdelegate.rb
@@ -1,8 +1,8 @@
 class Collabbdelegate < Formula 
     desc "A command line to create adapter delegates"
-    url "https://github.com/jobinlawrance/collabb-delegate/archive/v0.0.1.tar.gz" 
-    sha256 "27850ba6fedf68b90572e8463c8fdeb06f308b88260b8e85e30cb650b3968fe7"
-    version "0.0.1"
+    url "https://github.com/jobinlawrance/collabb-delegate/archive/v0.0.2.tar.gz" 
+    sha256 "83d088c364a04115f2e64b9ee855d97f3f0ae84ab3687f61c2b66bedfc7ec33d"
+    version "0.0.2"
   
     bottle :unneeded
   
