cb42f5247f7d088267d837f56b4e63bb71d3aadc
Jobin Lawrance <jobinlawrance@gmail.com>
2018-07-04 15:40:36 +0530

    Update sha256 of CollabbDelegate

--- Formula/collabbdelegate.rb
+++ Formula/collabbdelegate.rb
@@ -1,7 +1,7 @@
 class Collabbdelegate < Formula 
     desc "A command line to create adapter delegates"
     url "https://github.com/jobinlawrance/collabb-delegate/archive/v0.0.3.tar.gz" 
-    sha256 "b6e70087605ee62c9f24c86a50eedbf47cc2ab5eb0291ab876f3ea6c1a0dd6ea"
+    sha256 "fc9d4589b7ebea2eecef02d6e548e056f8a79742928e94380a2701ab816817b4"
     version "0.0.3"
   
     bottle :unneeded
